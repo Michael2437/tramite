a4854b1861e718e07971bd32d055ff12dc366d32
Michael Moriano <michaeldison.24_@hotmail.com>
2021-07-08 10:41:32 -0500

    a

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php session_start();
 
 if(isset($_SESSION['usuario'])){
-  if($_SESSION['usuario']== "Mesa de Partes"){
+  if($_SESSION['usuario']== "Administrador"){
+    header('Location: Admin\indexAdmin.php');}
+  elseif($_SESSION['usuario']== "Mesa de Partes"){
     header('Location: MesaDePartes\indexMDP.php');}
     else{
       header('Location: Areas\indexAreas.php');
