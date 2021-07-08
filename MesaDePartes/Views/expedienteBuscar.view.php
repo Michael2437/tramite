4b97357519ee4c7f0df9febf05eeb4881d6fe98e
Michael Moriano <michaeldison.24_@hotmail.com>
2021-07-08 10:49:41 -0500

    f

--- MesaDePartes/Views/expedienteBuscar.view.php
+++ MesaDePartes/Views/expedienteBuscar.view.php
@@ -15,7 +15,7 @@
     <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
         <div class="app-header header-shadow">
             <div class="app-header__logo">
-                <img src="views/assets/logos/logo.png" alt="" class="logo-src"> 
+                <img src="../Views/assets/logos/logo.png" alt="" class="logo-src"> 
                 <div class="header__pane ml-auto">
                     <div>
                         <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
@@ -95,7 +95,7 @@
         <div class="app-main">
           <div class="app-sidebar sidebar-shadow">
               <div class="app-header__logo">
-              <img src="views/assets/logos/logo.png" alt="" class="logo-src">
+              <img src="../Views/assets/logos/logo.png" alt="" class="logo-src">
                   <div class="header__pane ml-auto">
                       <div>
                           <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
--- MesaDePartes/Views/usuarioModificar.view.php
+++ MesaDePartes/Views/usuarioModificar.view.php
@@ -15,7 +15,7 @@
     <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
         <div class="app-header header-shadow">
             <div class="app-header__logo">
-                <div class="logo-src"></div>
+            <img src="../Views/assets/logos/logo.png" alt="" class="logo-src">
                 <div class="header__pane ml-auto">
                     <div>
                         <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
@@ -373,7 +373,7 @@
         <div class="app-main">
           <div class="app-sidebar sidebar-shadow">
               <div class="app-header__logo">
-                  <div class="logo-src"></div>
+              <img src="../Views/assets/logos/logo.png" alt="" class="logo-src">
                   <div class="header__pane ml-auto">
                       <div>
                           <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
